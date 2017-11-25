5de294ee7ab426058e1aaee65e751f12e72d09ca
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-11-25 12:03:56 -0800

    Build user#index backend

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,10 +11,14 @@ class Api::UsersController < ApplicationController
     end
   end
 
-
   def show
     @user = User.find_by(id: params[:id])
     render "api/users/show"
+  end
+
+  def index
+    @users = User.all
+    render "api/users/index"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "static_pages#root"
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create, :show]
+    resources :users, only: [:create, :show, :index]
     resource :session, only: [:create, :destroy]
     resources :stories, except: [:new, :edit]
   end
