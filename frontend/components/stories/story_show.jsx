7e2dba556e55a5b359af4176811255efd4f92fd7
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-11-28 15:49:55 -0800

    Add image support to Story Show

--- frontend/components/stories/story_show.jsx
+++ frontend/components/stories/story_show.jsx
@@ -16,7 +16,13 @@ class StoryShow extends React.Component {
       return null;
     } else {
       let { title, body, img_url, author_id, updated_at } = this.props.story;
-      console.log(this.props);
+
+      let img =
+        "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/12108215_10103930124440943_5214808814929508094_n.jpg?oh=1ef4f0bb2b9936d55580ca680010a434&oe=5A95C236";
+      if (img_url === "story_default.png") {
+        img_url = img;
+      }
+
       return (
         <div className="article-flex-container">
           <div className="article-flex-child follows-container">
@@ -25,7 +31,9 @@ class StoryShow extends React.Component {
           <div className="article-title article-flex-child">
             <h2 className="title">{title}</h2>
           </div>
-          <div className="article-img article-flex-child">{img_url}</div>
+          <div className="article-img article-flex-child">
+            <img src={img_url} />
+          </div>
           <div className="article-text-container article-flex-child">
             <pre className="article-text">{body}</pre>
           </div>
