a54ac8b28cddee57ac5dfa91edc70dba552a397b
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-12-01 14:03:45 -0800

    Finish production prep part 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,48 @@
-var path = require('path');
+var path = require("path");
+var webpack = require("webpack");
+
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    "process.env": {
+      NODE_ENV: JSON.stringify("production")
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === "production" ? prodPlugins : devPlugins
+);
 
 module.exports = {
-  entry: './frontend/abstract.jsx',
+  context: __dirname,
+  entry: "./frontend/abstract.jsx",
   output: {
-    filename: './app/assets/javascripts/bundle.js',
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
         test: [/\.jsx?$/, /\.js?$/],
         exclude: /(node_modules)/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
-          presets: ['es2015', 'react']
+          presets: ["es2015", "react"]
         }
       }
     ]
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   resolve: {
-    extensions: ['.js', '.jsx', '*']
+    extensions: [".js", ".jsx", "*"]
   }
 };
