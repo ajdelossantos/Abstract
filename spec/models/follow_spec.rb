09d699fec4ee3c75c7ea455c572d2e3e34cff29c
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-04-21 13:24:56 -0700

    Finish follow_spec.rb; finish model testing

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -13,4 +13,10 @@ require 'rails_helper'
 
 RSpec.describe Follow, type: :model do
 
+  # Bugged in current version of RSpec
+  # it { should validate_uniqueness_of(:follower).scoped_to(:followee) }
+
+  it { should belong_to(:follower) }
+  it { should belong_to(:followee) }
+
 end
