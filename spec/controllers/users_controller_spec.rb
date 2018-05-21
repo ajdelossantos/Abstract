a18350905d56cf6849b27a8d7643b92690a76021
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-05-21 13:30:40 -0700

    Commented out users controller tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+# RSpec.describe Api::UsersController, type: :controller do
+#   describe 'GET #index' do
+#     it 'renders the index users template' do
+#       get :index
+#       expect(response).to render_template('api/users/index')
+#     end
+#   end
+# end
